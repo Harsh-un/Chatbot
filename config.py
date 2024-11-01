fc2266bdc1430abd95813d4c511805970b86fd51
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-11-01 22:45:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "26516344"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "7a3f7d55d89476a15a62b4dd39062556"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "1808943146")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = getenv("OWNER_ID", "6253265083")
+SUPPORT_GRP = "unb_support"
+UPDATE_CHNL = "unb_info"
+OWNER_USERNAME = "harsh_un"
 
